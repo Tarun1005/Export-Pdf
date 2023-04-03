630be125fbcc26f16ebfd4345f2ce69c4fed2150
Tarun <Tarunankam05@gmail.com>
2023-04-03 09:51:34 +0530

    pdf

---
+++ src/main/java/com/tarun/Pdf/Controller/PdfExportController.java
@@ -0,0 +1,35 @@
+package com.tarun.Pdf.Controller;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.tarun.Pdf.Service.PDFGeneratorService;
+
+@Controller
+public class PdfExportController {
+
+	@Autowired
+    PDFGeneratorService pdfGeneratorService;
+ 
+    @GetMapping("/openpdf/export")
+    public void createPDF(HttpServletResponse response) throws IOException {
+        response.setContentType("application/pdf");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd:hh:mm:ss");
+        String currentDateTime = dateFormatter.format(new Date());
+ 
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=pdf_" + currentDateTime + ".pdf";
+        response.setHeader(headerKey, headerValue);
+ 
+        pdfGeneratorService.export(response);
+    }
+}
+
